7abf8e59458029cbd2065becbd029c7eff8a3f21
Driti Virani <viranidriti@gmail.com>
2025-07-26 15:52:06 -0700

    Default theme copied to new theme v.01

---
+++ manifest.php
@@ -0,0 +1,45 @@
+<?php
+/*
+Gibbon: the flexible, open school platform
+Founded by Ross Parker at ICHK Secondary. Built by Ross Parker, Sandra Kuipers and the Gibbon community (https://gibbonedu.org/about/)
+Copyright © 2010, Gibbon Foundation
+Gibbon™, Gibbon Education Ltd. (Hong Kong)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+//This file describes the module, including database tables
+
+//Basic variables
+$name = 'teachnova';
+$description = "TeachNova boasts a clean, spacious, and intuitive design, reminiscent of Apple's iconic aesthetic. Our light and modern theme, featuring white and light gray backgrounds with high-contrast dark text, ensures optimal readability. Vibrant blue and teal accents highlight key information and calls-to-action, adding a touch of energy to your daily tasks. With clear typography (Inter font), intuitive icons, and well-defined sections, TeachNova offers an uncluttered, professional, and effortlessly user-friendly experience.";
+$version = '1.0.00';
+$author = 'Driti Virani';
+$url = '';
+$responsive = 'Y';
+
+// Optionally define a set of theme colours that this theme supports.
+$themeColours = [
+    'purple' => __('Purple'),
+    'indigo' => __('Indigo'),
+    'blue'   => __('Blue'),
+    'sky'    => __('Sky'),
+    'teal'   => __('Teal'),
+    'green'  => __('Green'),
+    'yellow' => __('Yellow'),
+    'orange' => __('Orange'),
+    'red'    => __('Red'),
+    'pink'   => __('Pink'),
+    'fuchsia' => __('Violet'),
+];
