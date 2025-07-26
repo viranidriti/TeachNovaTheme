17bcd628df107aab709ee22ff6841d8e24fe9f16
Driti Virani <viranidriti@gmail.com>
2025-07-26 16:02:21 -0700

    Manifest changes.

--- manifest.php
+++ manifest.php
@@ -22,11 +22,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //This file describes the module, including database tables
 
 //Basic variables
-$name = 'teachnova';
+$name = 'TeachNovaTheme';
 $description = "TeachNova boasts a clean, spacious, and intuitive design, reminiscent of Apple's iconic aesthetic. Our light and modern theme, featuring white and light gray backgrounds with high-contrast dark text, ensures optimal readability. Vibrant blue and teal accents highlight key information and calls-to-action, adding a touch of energy to your daily tasks. With clear typography (Inter font), intuitive icons, and well-defined sections, TeachNova offers an uncluttered, professional, and effortlessly user-friendly experience.";
 $version = '1.0.00';
 $author = 'Driti Virani';
-$url = '';
+$url = 'https://github.com/viranidriti/TeachNovaTheme.git';
 $responsive = 'Y';
 
 // Optionally define a set of theme colours that this theme supports.
