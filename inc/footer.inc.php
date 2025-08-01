6e50b95b52217a35c7aa55b22074e0f3e51096f0
Driti Virani <viranidriti@gmail.com>
2025-08-01 09:59:45 -0700

    Minor UX changes

--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -9,14 +9,14 @@
                     </a>
                 </div>
                 <nav class="flex flex-wrap justify-center gap-x-6 gap-y-2" aria-label="Footer">
-                    <a href="/TeachNova/index.php" class="text-base text-gray-600 hover:text-gray-900 transition-colors">Login</a>
-                    <a href="/TeachNova/contact.php" class="text-base text-gray-600 hover:text-gray-900 transition-colors">Contact Us</a>
-                    <a href="/TeachNova/about.php" class="text-base text-gray-600 hover:text-gray-900 transition-colors">About Us</a>
+                    <a href="/TeachNova/index.php" class="text-base text-blue-900 hover:text-blue-500 transition-colors">Login</a>
+                    <a href="/TeachNova/contact.php" class="text-base text-blue-900 hover:text-blue-500 transition-colors">Contact Us</a>
+                    <a href="/TeachNova/about.php" class="text-base text-blue-900 hover:text-blue-500 transition-colors">About Us</a>
                 </nav>
             </div>
             <div class="mt-12 pt-8 border-t border-gray-200 flex flex-col sm:flex-row-reverse items-center justify-between gap-y-4">
                 <div class="flex space-x-6">
-                    <a href="https://www.linkedin.com/in/driti-virani-7a6048315/" class="text-gray-500 hover:text-gray-800 transition-colors" target="_blank">
+                    <a href="https://www.linkedin.com/in/driti-virani-7a6048315/" class="text-blue-500 hover:text-blue-900 transition-colors" target="_blank">
                          <span class="sr-only">LinkedIn</span>
                         <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fill-rule="evenodd" d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" clip-rule="evenodd"/></svg>
                     </a>
